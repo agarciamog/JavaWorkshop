478ad2662c1d14789c2e28364f056fc47845f837
Alberto Garcia <alberto.garcia@incontact.com>
2017-08-28 14:49:15 -0600

    Testing Intellij and Git.

--- src/io/albertogarcia/javaWorkshop/JavaWorkshop.java
+++ src/io/albertogarcia/javaWorkshop/JavaWorkshop.java
@@ -3,8 +3,7 @@ package io.albertogarcia.javaWorkshop;
 public class JavaWorkshop {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		System.out.println("Hello Git!");
 	}
-
 }
